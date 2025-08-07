1fcb13de5f378c0e6aab7db973cb7217a39ef613
AnuragLawaniya0001 <anuraglawaniya0001@gmail.com>
2025-08-07 22:36:01 +0530

    Registeration backend/API

--- Backend/Stock_prediction/settings.py
+++ Backend/Stock_prediction/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG' , defualt =False , cast=bool)
+DEBUG = config('DEBUG' , default =False , cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 
 # Application definition
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'accounts',
+    'api',
 ]
 
 MIDDLEWARE = [
